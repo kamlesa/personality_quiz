3bc894e6abbace20848963053f62a95aa0868147
anika <akam0020@student.monash.edu>
2024-05-17 00:20:39 +1000

    Got everything working just need to input proper questions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# personality_quiz
-Simple Personality Quiz
+# Farmer's Foragables Quiz
+Hello!
+This is a mini project inspired by the currently trending MBTI quizzes ^-^
+Made by Me!!
+
+I have recently been playing a lot of Stardew Valley, and thought that would be a cool setting for my quiz, so a lot of the results and questions will be inspired by the game.
+
+Current Link: NONE.
+
+It is still in progress but stick around!
+
---
+++ script.js
@@ -0,0 +1,351 @@
+//ALL RELATED JS.
+const questions = [
+    {
+        question: "Q1/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Press for F",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Press for T",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: +2, F: 0, J: 0, P: 0},
+        },
+        option3: {
+                text: "Press for J",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 4, P: 0},
+        },
+    },{
+        question: "Q2/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q3/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q4/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q5/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q6/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q7/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q8/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q9/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q10/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q11/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    },{
+        question: "Q12/12: INSERT QUESTION",
+        //image: "images/Q1.gif",
+        option1: {
+                text: "Answer 1",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option2: {
+                text: "Answer 2",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+        option3: {
+                text: "Answer 3",
+                scores: { E: 0, I: 0, S: 0, N: 0, T: 0, F: +1, J: 0, P: 0},
+        },
+    }
+]
+
+currentQ = 0;
+scores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0};
+button = document.createElement("button");
+button2 = document.createElement("button");
+button3 = document.createElement("button");
+question = document.createElement("p");
+
+
+//method to start the quiz
+function startQuiz() {
+    document.getElementById("start").style.display = "none";
+    currentQ = 0;
+    button.style.display = "inline";
+    button2.style.display = "inline";
+    button3.style.display = "inline";
+    debugHandlers();
+    displayQuestion();
+} 
+
+function displayQuestion() {
+    const quizDiv = document.getElementById("quiz");
+
+    const item = questions[currentQ];
+
+    question.innerText = item.question;
+
+    quizDiv.appendChild(question)
+
+    createHandlers(item);
+    
+    button.innerText = item.option1.text;
+    quizDiv.appendChild(button);
+    quizDiv.appendChild(document.createElement("p"));
+
+
+    button2.innerText = item.option2.text;
+    quizDiv.appendChild(button2);
+    quizDiv.appendChild(document.createElement("p"));
+
+
+    button3.innerText = item.option3.text;
+    quizDiv.appendChild(button3);
+}
+
+function debugHandlers(){
+        // Define the new event handler
+        buttonClickHandler = function() {
+            console.log("HANDLER")
+        };
+    
+        // Define the new event handler
+        button2ClickHandler = function() {
+            console.log("HANDLER")
+        };
+
+    
+        // Define the new event handler
+        button3ClickHandler = function() {
+            console.log("HANDLER")
+        };
+    
+        button.addEventListener("click", buttonClickHandler);
+        button2.addEventListener("click", button2ClickHandler);
+        button3.addEventListener("click", button3ClickHandler);
+}
+
+function createHandlers(item){
+    // Remove the old event listener if it exists
+    if (buttonClickHandler) {
+        button.removeEventListener("click", buttonClickHandler);
+    }
+
+    // Define the new event handler
+    buttonClickHandler = function() {
+        optionClicked(item.option1.scores);
+    };
+
+    // Remove the old event listener if it exists
+    if (button2ClickHandler) {
+        button2.removeEventListener("click", button2ClickHandler);
+    }
+
+    // Define the new event handler
+    button2ClickHandler = function() {
+        optionClicked(item.option2.scores);
+    };
+
+    // Remove the old event listener if it exists
+    if (button3ClickHandler) {
+        button3.removeEventListener("click", button3ClickHandler);
+    }
+
+    // Define the new event handler
+    button3ClickHandler = function() {
+        optionClicked(item.option3.scores);
+    };
+
+    button.addEventListener("click", buttonClickHandler);
+    button2.addEventListener("click", button2ClickHandler);
+    button3.addEventListener("click", button3ClickHandler);
+
+}
+
+function optionClicked(results){
+    console.log("Selected Results",results)
+    currentQ+=1;
+    console.log("Current Question",currentQ)
+
+    for (let prop in results) {
+        if (results.hasOwnProperty(prop) && scores.hasOwnProperty(prop)) {
+            scores[prop] = scores[prop] + results[prop];
+        }
+    }
+
+    console.log("Updates Results", scores)
+
+
+    if (currentQ>=questions.length){
+        displayResult();
+    }else{
+        displayQuestion();
+    }
+    
+}
+
+function displayResult(){
+    button2.style.display = "none";
+    button3.style.display = "none";
+
+    
+    mbti = "";
+    if(scores.E >= scores.I){
+        mbti += "E"
+    }else{
+        mbti += "I"
+    }
+
+    if(scores.N > scores.S){
+        mbti += "N"
+    }else{
+        mbti += "S"
+    }
+
+    if(scores.T > scores.F){
+        mbti += "T"
+    }else{
+        mbti += "F"
+    }
+
+    if(scores.P >= scores.J){
+        mbti += "P"
+    }else{
+        mbti += "J"
+    }
+    
+    question.innerText = "Your result is PUFFERFISH " + mbti;
+    console.log(mbti);
+    
+    button.innerText = "Back to Home"
+    button.addEventListener("click",function() {
+        scores = { E: 0, I: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0};
+        location.reload();
+        });
+    quizDiv.appendChild(button);
+    quizDiv.appendChild(document.createElement("p"));
+}
